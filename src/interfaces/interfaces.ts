ed8c26fcc3e4c70f81bf16ecaba07adf5b425394
DarlanDuarte <darlan.duarte.m@gmail.com>
2023-08-28 15:27:02 -0300

    implementando update e login novamente

--- src/interfaces/interfaces.ts
+++ src/interfaces/interfaces.ts
@@ -1,7 +1,13 @@
 export interface IDataCreate {
-  id: number;
-  name: string;
-  lastName: string;
-  email: string;
-  password: string;
+  id: number
+  name: string
+  lastName: string
+  email: string
+  password: string
+}
+export interface IUpdateUser {
+  id: number
+  name: string
+  lastName: string
+  email: string
 }
